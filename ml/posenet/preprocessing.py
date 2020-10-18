78881d33e9ff4b7658015f6d01b2827262b9c3c7
William Shiao <willshiao@gmail.com>
2020-10-17 18:21:35 -0700

    change normalize function

--- ml/posenet/preprocessing.py
+++ ml/posenet/preprocessing.py
@@ -1,24 +1,29 @@
 from sklearn import preprocessing
 import numpy as np
+import posenet
 
-def Normalize(pose_scores, keypoint_scores, keypoint_coords):
-  drop = [] # hold indices to drop
-  for pi in range(len(pose_scores)):
-    if pose_scores[pi] == 0.:
-        break
-    for ki, (s, c) in enumerate(zip(keypoint_scores[pi, :], keypoint_coords[pi, :, :])):
-      # filter out too low of score
-      if (s > 0.2):
-          drop.append(ki)
 
-  # trying to drop coordinate that we don't want        
-  keypoint_coords = np.drop(keypoint_coords, drop)
+def Normalize(pose_scores, keypoint_scores, keypoint_coords, thresh=0.1):
+    keypoint_scores = keypoint_scores.reshape((17, -1))
+    keypoint_coords = keypoint_coords.reshape((17, 2))
 
-  minVal= np.amin(keypoint_coords, axis=0) # holds min x and y values of the coordinates
-  # scale coodrinates
-  for i in range(len(keypoint_coords)):
-      for j in range(len(keypoint_coords[i])):
-          keypoint_coords[i][j] -= minVal
-  # normalize
-  normalized_keypoint_coords  = preprocessing.normalize(keypoint_coords, norm='l2')
-  return normalized_keypoint_coords
+    # Step 1: filter out bad scores
+    mask = (keypoint_scores.ravel() > thresh)
+    if not np.any(mask):
+        return {}
+
+    # Step 2: Crop
+    min_x = np.min(keypoint_coords[mask, 0])
+    min_y = np.min(keypoint_coords[mask, 1])
+    keypoint_coords[:, 0] -= min_x
+    keypoint_coords[:, 1] -= min_y
+
+    # Step 3: Normalize
+    normalized_coords = preprocessing.normalize(keypoint_coords, norm='l2')
+
+    # Step 4: Convert to dict
+    output = {}
+    for i in range(17):
+        if mask[i]:
+            output[posenet.PART_NAMES[i]] = (normalized_coords[i, 0], normalized_coords[i, 1])
+    return output
